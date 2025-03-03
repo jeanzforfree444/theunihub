03fd6ef225178019792741e8f1f2d47ed380d11d
“Aaron <2881785H@student.gla.ac.uk>
2025-03-03 11:11:08 +0000

    Registration installation

--- theunihub/urls.py
+++ theunihub/urls.py
@@ -3,8 +3,17 @@ from django.urls import path, include, reverse
 from main import views
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.backends.simple.views import RegistrationView
+
+class HubRegistrationView(RegistrationView):
+    
+    def get_success_url(self, user):
+    
+        return reverse('rango:register_profile')
 
 urlpatterns = [
     path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/register/', HubRegistrationView.as_view(), name='registration_register'),
+    path('accounts/', include('registration.backends.simple.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
